97568be6a9fcbaa966ccc875a313a910dbeb7bc6
Priyanshu Negi <negips11april@gmail.com>
2026-05-19 23:15:00 +0530

    fixed production lead api

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 from .models import Lead
 from .serializers import LeadSerializer
@@ -8,17 +8,17 @@ from .serializers import LeadSerializer
 class LeadListCreateView(generics.ListCreateAPIView):
 
     serializer_class = LeadSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def get_queryset(self):
         return Lead.objects.all()
 
     def perform_create(self, serializer):
-        serializer.save(assigned_to=self.request.user)
+        serializer.save()
 
 
 class LeadDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
